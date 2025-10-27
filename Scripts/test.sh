c88b6de62844021ef633d4cbad081ba4506ab512
devitoSans <devitosanjaya11@gmail.com>
2025-10-28 03:46:43 +1100

    fix test.sh bug

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -7,4 +7,3 @@ CURRENT_FILE_PATH=$1
 CURRENT_FILE_FOLDER=`dirname $CURRENT_FILE_PATH`
 
 C:/msys64/mingw64/bin/g++.exe -g $(find $CURRENT_FILE_FOLDER -type f -name '*.cpp' -print) -Wall -Wextra -lSplashKit -L"$PROJECT_PATH/lib" -I"$PROJECT_PATH/include" -lcatch_amalgamated -o "${CURRENT_FILE_PATH%.cpp}.exe"
-${CURRENT_FILE_PATH%.cpp}.exe
