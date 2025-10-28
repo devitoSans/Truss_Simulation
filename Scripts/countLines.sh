bf44f4deb52fbeaff13e3b27657af26186682a7a
devitoSans <devitosanjaya11@gmail.com>
2025-10-29 02:53:47 +1100

    fix bug

--- Scripts/countLines.sh
+++ Scripts/countLines.sh
@@ -3,4 +3,5 @@
 PROJECT_PATH=`pwd`
 PROJECT_NAME=`basename $PROJECT_PATH`
 
-find ${PROJECT_PATH}/sources -type f | xargs wc -l
+find ${PROJECT_PATH}/sources \( -name '*.cpp' -o -name '*.hpp' \) ! -name "tester.cpp" -type f | xargs wc -l
+# find ${PROJECT_PATH}/sources \( -name '*.cpp' -o -name '*.hpp' \) -type f | xargs wc -l
